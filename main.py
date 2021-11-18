95bbc1992832cae3480b344a148b5a3b2f8b5ceb
Nik Pirc <nik@oaa.si>
2021-11-18 17:14:44 +0100

    added something for heroku

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from sqla_wrapper import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-db = SQLAlchemy("sqlite:///db.sqlite")
+db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
+db = SQLAlchemy(db_url)
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String, unique=False)
